945d0669ef872e6188d928cb52dc31117d54e9d2
Simplecodez <102396467+Simplecodez@users.noreply.github.com>
2023-03-08 13:31:04 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,8 @@ from datetime import datetime
 
 class BaseModel:
     """
-    A base model class that defines all attributes and methods for other objects and classes to inherit from"""
+    A base model class that defines all attributes and methods for other objects and classes to inherit from
+    """
     def __init__(self):
         self.id = str(uuid.uuid4())
         #self.created_at = datetime.datetime.now()
