b5b085ac8f18257f885c725035c1152caef654fb
Divinelewis <divinitysoft0@gmail.com>
2023-03-10 12:42:08 +0100

    The console file

--- console.py
+++ console.py
@@ -1,15 +1,32 @@
 import cmd
+from shlex import split as split
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
-    def do_quit(self, arg):
+    def do_quit(self, line):
         """Quit command to exit the program"""
         return True
 
-    def do_EOF(self, arg):
+    def do_EOF(self, line):
         """EOF command to exit the program"""
         return True
+
+    def do_create(self, line):
+        """Creates a new instance of BaseModel, saves it (to the JSON file)
+        and prints the id"""
+        splitline = split(line)
+        if len(line) == 0:
+            print("** class name missing **")
+            return
+        try:
+            new_instance = eval(line[0])()
+            new_instance.save()
+            print(new_instance.id)
+        except NameError:
+            print("** class doesn't exist **")
+            return
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
